36729bb3a1fa6dfc35d3bea507c16d8d21f0de6a
cathyiic <103155841+cathyiic@users.noreply.github.com>
2022-06-23 10:29:11 -0700

    reading int

---
+++ reading.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#defining an int variable then reading variable using scanf, and then printing that variable 
+
+int main(){
+  int x;
+  scanf("%d", &x);
+  
+  print("Here is your int variable : %d", x);
+}
