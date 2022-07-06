383b3a5c32b43a3b72e0505329fdbab779086522
cathyiic <103155841+cathyiic@users.noreply.github.com>
2022-07-05 22:03:43 -0700

    these need to be deleted from repo

--- test1.c
+++ test1.c
@@ -1,9 +1,27 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
+/*
 int main(){
-    int x = 4;
-    int y = 5;
-    x = y > x ? 3 : 4;
-    printf("%d", x);
+printf("My name is Catherine Ichwan.\n");
+int num;
+printf("Please enter a num : ");
+scanf("%d", &num);
+printf("Here is your num : %d\n", num);
+}*/
+
+/*int main(){
+    float num;
+    printf("Please enter your favorite number : ");
+    scanf("%f", &num);
+    if (num < 0){
+        printf("Your number is negative.\n");
+    }
+    else if (num <= 100){
+        printf("Your number is mid.\n");
+    }
+    else {
+        printf("That is a big number.\n");
+    }
 }
+*/
